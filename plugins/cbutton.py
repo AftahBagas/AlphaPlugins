1be4b0847790a8ae23b4d565f4f2fba09d7c0422
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-08-13 20:58:08 +0530

    added  -c flag in ibutton

--- plugins/cbutton.py
+++ plugins/cbutton.py
@@ -1,13 +1,17 @@
 """ Create Buttons Through Bots """
 
-# By @Krishna_Singhal
+# IMPROVED BY code-rgb
+# By @krishna_singhal
 
 from pyrogram.errors.exceptions.bad_request_400 import (
     UserIsBot, BadRequest, MessageEmpty)
 
 from userge import userge, Config, Message, get_collection
 from userge.utils import parse_buttons as pb
+import re
 
+BTN = r"\[([^\[]+?)\](\[buttonurl:(?:/{0,2})(.+?)(:same)?\])|\[([^\[]+?)\](\(buttonurl:(?:/{0,2})(.+?)(:same)?\))"
+BTNX = re.compile(BTN)
 
 @userge.on_cmd("cbutton", about={
     'header': "Create buttons Using bot",
@@ -45,13 +49,30 @@ async def create_button(msg: Message):
 
 
 @userge.on_cmd("ibutton", about={
-    'header': "Create buttons Using bot"})
+     'header': "Creates Button Via Inline bot in any chat",
+     'flags': {
+     "c": "replace all () bracket to [] "},
+    'usage': "{tr}ibutton [ reply to message]\n"
+              "{tr}ibutton -c [ reply to message]"})
 async def inline_buttons(message: Message):
     replied = message.reply_to_message
     if not (replied and replied.text):
-        await message.err("Reply a text Msg")
-        return
+         await message.err("Reply a text Msg")
+         return
     text = replied.text
+    # Lmao Weird code but works xD
+    if '-c' in message.flags:
+        unmatch = re.sub(BTN, '', text)
+        textx = ""
+        for m in BTNX.finditer(text):
+            if m.group(1):
+                word = m.group(0)
+            else:
+                change = m.group(6).replace('(', '[').replace(')', ']')
+                word = "[" + m.group(5) + "]"
+                word += change
+            textx += word 
+        text = unmatch + textx
     BUTTON_BASE = get_collection("TEMP_BUTTON")
     BUTTON_BASE.insert_one({'msg_data': text})
     bot = await userge.bot.get_me()
@@ -59,5 +80,4 @@ async def inline_buttons(message: Message):
     await userge.send_inline_bot_result(chat_id=message.chat.id,
                                         query_id=x.query_id,
                                         result_id=x.results[1].id)
-
     await BUTTON_BASE.drop()

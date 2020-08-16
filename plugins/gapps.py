89cf6af40212dbfb816ca2664e0906bd6acac0db
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-08-16 18:43:44 +0530

    New Module Gapps.py

---
+++ plugins/gapps.py
@@ -0,0 +1,13 @@
+# BY code-rgb
+"""Gapps via inline bot"""
+from userge import userge, Message
+@userge.on_cmd("gapps", about={
+    'header': "Get Gapps"}, allow_channels=False)
+async def alive_inline(message: Message):
+    await message.edit("`🔍 Finding Latest Gapps...`")
+    bot = await userge.bot.get_me()
+    x = await userge.get_inline_bot_results(bot.username, "gapps")
+    await userge.send_inline_bot_result(chat_id=message.chat.id,
+                                        query_id=x.query_id,
+                                        result_id=x.results[1].id)
+    await message.delete()

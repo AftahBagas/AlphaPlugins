01047462da061ef649ad4f5c64fab19fb10066cc
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2020-11-13 21:37:04 +0530

    Update time.py

--- plugins/time.py
+++ plugins/time.py
@@ -1,54 +1,105 @@
+# Ported From [PaperPlane Userbot](https://github.com/RaphielGang/Telegram-Paperplane)
+
+""" Userbot module for getting the date
+    and time of any country or the userbot server.  """
+
 import os
 from datetime import datetime as dt
-
-from pytz import timezone
 from userge import Message, userge
+from pytz import country_names as c_n
+from pytz import country_timezones as c_tz
+from pytz import timezone as tz
 
-LOG = userge.getLogger(__name__)  # logger object
 
 COUNTRY_CITY = os.environ.get("COUNTRY_CITY", None)
 
 
+async def get_tz(con):
+    """ Get time zone of the given country. """
+    if "(Uk)" in con:
+        con = con.replace("Uk", "UK")
+    if "(Us)" in con:
+        con = con.replace("Us", "US")
+    if " Of " in con:
+        con = con.replace(" Of ", " of ")
+    if "(Western)" in con:
+        con = con.replace("(Western)", "(western)")
+    if "Minor Outlying Islands" in con:
+        con = con.replace("Minor Outlying Islands", "minor outlying islands")
+    if "Nl" in con:
+        con = con.replace("Nl", "NL")
+
+    for c_code in c_n:
+        if con == c_n[c_code]:
+            return c_tz[c_code]
+    try:
+        if c_n[con]:
+            return c_tz[con]
+    except KeyError:
+        return
+
+
+
 @userge.on_cmd(
-    "dt",
+    "dt(?: |$)(.*)(?<![0-9])(?: |$)([0-9]+)?",
     about={
-        "header": "Get the time and date of a City/Country/Timezone.",
-        "flags": {"-l": "Gives list of all Country/City Combos for Heroku Config"},
-        "usage": "Use {tr}dt to show the Time & Date of your predefined City\n"
-        "Use {tr}dt -l to display all TZ Combo's for the Config\n",
-        "examples": ["{tr}dt", "{tr}dt [Flag]"],
+        "header": "Get Date and Time of a country",
+        "description": "Get the Date and Time of a country. If a country has multiple timezones, it will list all of them and let you select one.",
+        "usage": "{tr}dt <country name/code> <timezone number>",
+        "examples": ["{tr}dt Russia 2"],
+        "default timezone" : "Choose from the <b><a href=\"https://pastebin.com/raw/0KSh9CMj\">Timezones Avaliable</a></b>\n and Set any of them in (<code>COUNTRY_CITY</code>) for your default timezone"
     },
-    del_pre=True,
 )
-async def grabTime(message: Message):
-    LOG.info("Starting Time command...")
-    defaultMessage = (
-        "<code>Below is a list of all the Timezones Avaliable</code> \n<a "
-        "href=https://pastebin.com/raw/0KSh9CMj>Click Here!</a>\n<code>Enter"
-        " one in your Heroku Config Under</code> (<code>COUNTRY_CITY</code>)\n"
-        "<code>Ex: America/Los_Angeles</code>"
-    )
+async def date_time_func(message: Message):
+    """ For .dt command, return the date and time of
+        1. The country passed as an argument,
+        2. The default userbot country,
+        3. The server where the userbot runs.
+    """
+    con = message.matches[0].group(1).title()
+    tz_num = message.matches[0].group(2)
+    d_form = "%d/%m/%y - %A"
+    c_name = None
+    t_form = "%I:%M %p"
 
-    if "l" in message.flags:
-        LOG.info("Time: List Flag Used: Giving TZ list...")
-        await message.edit(
-            defaultMessage, disable_web_page_preview=True, parse_mode="html", del_in=30
-        )
+    if len(con) > 4:
+        try:
+            c_name = c_n[con]
+        except KeyError:
+            c_name = con
+        timezones = await get_tz(con)
+    elif COUNTRY_CITY:
+        timezones = [COUNTRY_CITY]
+    else:
+        await message.edit(f"`It's` **{dt.now().strftime(t_form)}** on **{dt.now().strftime(d_form)}**  `here.`")
         return
 
-    if not COUNTRY_CITY:
-        LOG.info("Time: No Config Set")
-        await message.edit(
-            defaultMessage, disable_web_page_preview=True, parse_mode="html", del_in=30
-        )
+    if not timezones:
+        await message.err("Invaild country.", del_in=5)
         return
 
-    tz = COUNTRY_CITY
-    tzDateTime = dt.now(timezone(tz))
-    date = tzDateTime.strftime("%d-%m-%Y")
-    militaryTime = tzDateTime.strftime("%H:%M")
-    time = dt.strptime(militaryTime, "%H:%M").strftime("%I:%M %p")
+    if len(timezones) == 1:
+        time_zone = timezones[0]
+    elif len(timezones) > 1:
+        if tz_num:
+            tz_num = int(tz_num)
+            time_zone = timezones[tz_num - 1]
+        else:
+            return_str = f"`{c_name} has multiple timezones:`\n"
+
+            for i, item in enumerate(timezones):
+                return_str += f"`{i+1}. {item}`\n"
+
+            return_str += "\n`Choose one by typing the number "
+            return_str += "in the command.`\n"
+            return_str += f"Example: .dt {c_name} 2"
+
+            await message.edit(return_str)
+            return
+
+    dtnow = dt.now(tz(time_zone)).strftime(d_form)
+    dttime = dt.now(tz(time_zone)).strftime(t_form)
+
     await message.edit(
-        "It is currently " + time + " on " + date + " in " + tz.replace("_", " ")
-    )
-    LOG.info("Time: Command Finished Successfully")
+        f"`It's`  **{dttime}** `on` **{dtnow}**  `in {c_name} ({time_zone} timezone).`")
+        

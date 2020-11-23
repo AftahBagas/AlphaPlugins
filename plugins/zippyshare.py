5879fd62f01463e329f4cd4966a5277a9cd802c2
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-11-23 07:32:28 +0000

    pylint: auto fixes

--- plugins/zippyshare.py
+++ plugins/zippyshare.py
@@ -5,34 +5,44 @@
 import re
 
 import requests
+from userge import Message, pool, userge
 
-from userge import userge, Message, pool
 
-
-@userge.on_cmd("zippy", about={
-    'header': "generate Direct link of zippyshare url",
-    'usage': "{tr}zippy : [Zippyshare Link ]",
-    'examples': "{tr}zippy https://www10.zippyshare.com/v/dyh988sh/file.html"}, del_pre=True)
+@userge.on_cmd(
+    "zippy",
+    about={
+        "header": "generate Direct link of zippyshare url",
+        "usage": "{tr}zippy : [Zippyshare Link ]",
+        "examples": "{tr}zippy https://www10.zippyshare.com/v/dyh988sh/file.html",
+    },
+    del_pre=True,
+)
 async def zippyshare(message: Message):
     """ zippy to direct """
     url = message.input_str
     await message.edit("`Generating url ....`")
     try:
         direct_url, fname = await _generate_zippylink(url)
-        await message.edit(f"**Original** : {url}\n**FileName** : `{fname}`\n"
-                           f"**DirectLink** : {direct_url}\n\n"
-                           "**[HINT]** : use `.download [directLink]`",
-                           disable_web_page_preview=True)
+        await message.edit(
+            f"**Original** : {url}\n**FileName** : `{fname}`\n"
+            f"**DirectLink** : {direct_url}\n\n"
+            "**[HINT]** : use `.download [directLink]`",
+            disable_web_page_preview=True,
+        )
     except Exception as z_e:  # pylint: disable=broad-except
         await message.edit(f"`{z_e}`")
 
 
-_REGEX_LINK = r'https://www(\d{1,3}).zippyshare.com/v/(\w{8})/file.html'
-_REGEX_RESULT = (r'var a = (\d+);[\s\S]+document.getElementById\(\'dlbutton\'\).href'
-                 r' = "/d/\w{8}/.+/(.*)";')
-_HEADERS = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                          "AppleWebKit/537.36 (KHTML, like Gecko) Chrome"
-                          "/75.0.3770.100 Safari/537.36"}
+_REGEX_LINK = r"https://www(\d{1,3}).zippyshare.com/v/(\w{8})/file.html"
+_REGEX_RESULT = (
+    r"var a = (\d+);[\s\S]+document.getElementById\(\'dlbutton\'\).href"
+    r' = "/d/\w{8}/.+/(.*)";'
+)
+_HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome"
+    "/75.0.3770.100 Safari/537.36"
+}
 
 
 @pool.run_in_thread
@@ -50,5 +60,7 @@ def _generate_zippylink(url):
         if not match:
             raise ValueError("Invalid Response!")
         val, name = int(match.group(1)), match.group(2)
-        d_l = "https://www{}.zippyshare.com/d/{}/{}/{}".format(server, id_, val**3+3, name)
+        d_l = "https://www{}.zippyshare.com/d/{}/{}/{}".format(
+            server, id_, val ** 3 + 3, name
+        )
     return d_l, name

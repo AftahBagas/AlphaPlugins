9d3618c4d91e8f57e73f74434850546df80b657f
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-12-13 08:24:42 +0000

    pylint: auto fixes

--- plugins/spin.py
+++ plugins/spin.py
@@ -1,47 +1,45 @@
-from PIL import Image
-import shutil
 import os
-from userge.utils import runcmd, media_to_image
+import shutil
+
+from PIL import Image
+from userge import Message, userge
 from userge.plugins.utils.circle import crop_vid
-from userge import userge, Message
+from userge.utils import media_to_image, runcmd
 
-path = 'userge/xcache/rotate-disc/'
+path = "userge/xcache/rotate-disc/"
 
-@userge.on_cmd("spin", about={
-    'header': "Brr... 🌀",
-    "flags": {
+
+@userge.on_cmd(
+    "spin",
+    about={
+        "header": "Brr... 🌀",
+        "flags": {
             "-s": "Speed -> 1-6",
             "-c": "Spin Clockwise",
             "-r": "for round video",
         },
-    'description': "Reply to any media to spin",
-    'usage': '{tr}spin [flags] [reply to media]',
-    "examples": [
-        "{tr}spin",
-        "{tr}spin -s4 -c -r",
-    ]})
+        "description": "Reply to any media to spin",
+        "usage": "{tr}spin [flags] [reply to media]",
+        "examples": [
+            "{tr}spin",
+            "{tr}spin -s4 -c -r",
+        ],
+    },
+)
 async def spinn(message: Message):
     pic_loc = await media_to_image(message)
     if not pic_loc:
-        return await message.err('Reply to a valid media first', del_in=5)
-    await message.edit('🌀 `Tighten your seatbelts, sh*t is about to get wild ...`')
-    step_dict = {
-        "1": 1,
-        "2": 3,
-        "3": 6,
-        "4": 12,
-        "5": 24,
-        "6": 60
-    }
-    if '-s' in message.flags:
-        step = step_dict.get(message.flags['-s'], None)
+        return await message.err("Reply to a valid media first", del_in=5)
+    await message.edit("🌀 `Tighten your seatbelts, sh*t is about to get wild ...`")
+    step_dict = {"1": 1, "2": 3, "3": 6, "4": 12, "5": 24, "6": 60}
+    if "-s" in message.flags:
+        step = step_dict.get(message.flags["-s"], None)
         if not step:
             return await message.err("Not valid value for flag '-s'", del_in=5)
     else:
         step = 1
-  
 
-    spin_dir = -1 if '-c' in message.flags else 1
+    spin_dir = -1 if "-c" in message.flags else 1
 
     if not os.path.exists(path):
         os.mkdir(path)
@@ -56,17 +54,26 @@ async def spinn(message: Message):
     for k, nums in enumerate(range(1, 360, step), start=0):
         y = im.rotate(nums * spin_dir)
         y.save(os.path.join(path, "spinx%s.jpg" % k))
- 
+
     output_vid = os.path.join(path, "out.mp4")
-    frate = int(((90/59) * step) + (1680/59))
-    await runcmd(f"ffmpeg -framerate {frate} -i {path}spinx%d.jpg -c:v libx264 -pix_fmt yuv420p {output_vid}")
+    frate = int(((90 / 59) * step) + (1680 / 59))
+    await runcmd(
+        f"ffmpeg -framerate {frate} -i {path}spinx%d.jpg -c:v libx264 -pix_fmt yuv420p {output_vid}"
+    )
     if os.path.exists(output_vid):
         await message.delete()
-        if '-r' in message.flags:
+        if "-r" in message.flags:
             round_vid = os.path.join(path, "out_round.mp4")
             await crop_vid(output_vid, round_vid)
-            await message.client.send_video_note(message.chat.id, round_vid, reply_to_message_id=reply_id)
+            await message.client.send_video_note(
+                message.chat.id, round_vid, reply_to_message_id=reply_id
+            )
         else:
-            await message.client.send_animation(message.chat.id, output_vid, unsave=(not message.client.is_bot), reply_to_message_id=reply_id)
+            await message.client.send_animation(
+                message.chat.id,
+                output_vid,
+                unsave=(not message.client.is_bot),
+                reply_to_message_id=reply_id,
+            )
     os.remove(pic_loc)
     shutil.rmtree(path)

5ccbaf16a26430619bd0c7aab18ecc8b8ee5b569
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2021-01-05 10:45:05 +0530

    Fixed Crash cuz of  FloodWait

--- plugins/stats.py
+++ plugins/stats.py
@@ -6,8 +6,8 @@
 
 
 import time
-
-from pyrogram.errors import UserNotParticipant
+import asyncio
+from pyrogram.errors import UserNotParticipant, FloodWait
 from userge import Message, userge
 from userge.utils import mention_html, time_formatter
 
@@ -39,50 +39,54 @@ async def get_stats_(message: Message):
     channels = 0
     channels_admin = 0
     channels_creator = 0
-    async for dialog in userge.iter_dialogs():
-        unread_mentions += dialog.unread_mentions_count
-        unread_msg += dialog.unread_messages_count
-        chat_type = dialog.chat.type
-        if chat_type in ["bot", "private"]:
-            private_chats += 1
-            if chat_type == "bot":
-                bots += 1
+    try:
+        async for dialog in userge.iter_dialogs():
+            unread_mentions += dialog.unread_mentions_count
+            unread_msg += dialog.unread_messages_count
+            chat_type = dialog.chat.type
+            if chat_type in ["bot", "private"]:
+                private_chats += 1
+                if chat_type == "bot":
+                    bots += 1
+                else:
+                    users_ += 1
             else:
-                users_ += 1
-        else:
-            try:
-                is_admin = await admin_check(dialog.chat.id, owner.id)
-                is_creator = dialog.chat.is_creator
-            except UserNotParticipant:
-                is_admin = False
-                is_creator = False
-            if chat_type in ["group", "supergroup"]:
-                groups += 1
-                if is_admin:
-                    groups_admin += 1
-                if is_creator:
-                    groups_creator += 1
-            else:  # Channel
-                channels += 1
-                if is_admin:
-                    channels_admin += 1
-                if is_creator:
-                    channels_creator += 1
-        results = f"""
+                try:
+                    is_admin = await admin_check(dialog.chat.id, owner.id)
+                    is_creator = dialog.chat.is_creator
+                except UserNotParticipant:
+                    is_admin = False
+                    is_creator = False
+                if chat_type in ["group", "supergroup"]:
+                    groups += 1
+                    if is_admin:
+                        groups_admin += 1
+                    if is_creator:
+                        groups_creator += 1
+                else:  # Channel
+                    channels += 1
+                    if is_admin:
+                        channels_admin += 1
+                    if is_creator:
+                        channels_creator += 1
+    except FloodWait as e:
+        await asyncio.sleep(e.x + 5)
+
+    results = f"""
 📊 <b><u>Telegram Stats</u></b>
 👤 User:  <b>{u_mention}</b>
 
 <b>Private Chats:</b> <code>{private_chats}</code><code>
-   • Users: {users_}
-   • Bots: {bots}</code>
+    • Users: {users_}
+    • Bots: {bots}</code>
 <b>Groups:</b> <code>{groups}</code>
 <b>Channels:</b> <code>{channels}</code>
 <b>Admin in Groups:</b> <code>{groups_admin}</code><code>
-   ★ Creator: {groups_creator}
-   • Admin Rights: {groups_admin - groups_creator}</code>
+    ★ Creator: {groups_creator}
+    • Admin Rights: {groups_admin - groups_creator}</code>
 <b>Admin in Channels:</b> <code>{channels_admin}</code><code>
-   ★ Creator: {channels_creator}
-   • Admin Rights: {channels_admin - channels_creator}</code>
+    ★ Creator: {channels_creator}
+    • Admin Rights: {channels_admin - channels_creator}</code>
 <b>Unread Messages:</b> <code>{unread_msg}</code>
 <b>Unread Mentions:</b> <code>{unread_mentions}</code>
 """

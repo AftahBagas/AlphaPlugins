b1f0250a95c7578720d19164d87f0b6c21214cc2
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2021-01-19 20:41:37 +0530

    Update profile.py

--- plugins/profile.py
+++ plugins/profile.py
@@ -1,15 +1,18 @@
 """ All Profile Settings for User """
 
 # del_pfp by Phyco-Ninja
+# rewrote poto by code-rgb
 
 import os
 from datetime import datetime
-
+from pyrogram.types import InputMediaPhoto
 from pyrogram.errors import (
     AboutTooLong,
     UsernameNotOccupied,
     UsernameOccupied,
     VideoFileInvalid,
+    BadRequest,
+    FloodWait
 )
 from userge import Config, Message, userge
 from userge.utils import progress
@@ -407,26 +410,86 @@ async def revert_(message: Message):
     await message.edit("```Profile is Successfully Reverted...```", del_in=3)
 
 
+async def chat_type_(input_chat):
+    try:
+        chat_ = await message.client.get_chat(input_chat)
+    except (BadRequest, IndexError) as e:
+        return str(e), 0, 0
+    return chat_.type, chat_.id, chat_.photo.big_file_id
+
+async def send_single(chat_id, peer_id, pos, reply_id):
+    pic_ = await message.client.get_profile_photos(peer_id, limit=min(pos_, 100))
+    if len(pic_) != 0:
+        await message.client.send_photo(chat_id, photo=pic_.pop().file_id, reply_to_message_id=reply_id)
+
+
 # photo grabber
 @userge.on_cmd(
     "poto",
     about={
-        "header": "upload the photo of replied person or chat",
-        "usage": "upload the photo of replied person or chat",
-        "examples": ".poto",
+        "header": "use this plugin to get photos of user or chat",
+        "description": "fetches upto 100 photos of a user or chat",
+        "flags": {"-l": "limit, max. 100", "-p": "for photo at particular postion"},
+        "examples": [
+            "{tr}poto",
+            "{tr}poto [reply to chat message]",
+            "{tr}poto @midnightmadwalk -p5  (i.e sends photo at position 5)",
+            "{tr}poto @midnightmadwalk -l5  (i.e sends an album of first 5 photos)",
+        ],
     },
-    allow_channels=False,
 )
-async def photograb(message: Message):
-    if message.reply_to_message and message.reply_to_message.from_user.photo:
-        getid = message.reply_to_message.from_user.photo.big_file_id
-        getphoto = await message.client.download_media(getid)
-        await message.client.send_photo(message.chat.id, photo=getphoto)
-        os.remove(getphoto)
-    elif message.chat.photo and not message.reply_to_message:
-        phid = message.chat.photo.big_file_id
-        ppo = await message.client.download_media(phid)
-        await message.client.send_photo(message.chat.id, photo=ppo)
-        os.remove(ppo)
+async def poto_x(message: Message):
+    chat_id = message.chat.id
+    reply = message.reply_to_message
+    reply_id = reply.message_id if reply else None
+    if reply:
+        input_ = reply.from_user.id
+    elif message.filtered_input_str:
+        input_ = message.filtered_input_str.strip()
+        if len(input_.split()) != 1:
+            await message.err("provide a valid ID or username", del_in=5)
+            return
     else:
-        await message.err("Didnt Find Anything !")
+        input_ = chat_id
+    type_, peer_id, f_id = await chat_type_(input_)
+    if peer_id == 0:
+        await message.err(type_, del_in=7)
+        return
+    if type_ in ("group", "supergroup", "channel") and message.client.is_bot:
+        photo_ = await userge.bot.download_media(f_id)
+        await userge.bot.send_photo(chat_id, photo_, reply_to_message_id=reply_id)
+        os.remove(photo_)
+        return
+    flags_ = message.flags
+    if "-p" in flags_:
+        pos_ = flags_.get("-p", 0)
+        if not str(pos_).isdigit():
+            await message.err('"-p" Flag only takes integers', del_in=5)
+            return
+        await send_single(chat_id=chat_id, peer_id=peer_id, pos=int(pos_), reply_id=reply_id)
+    elif "-l" in flags_:
+        get_l = flags_.get("-l", 0)
+        if not str(get_l).isdigit():
+            await message.err('"-l" Flag only takes integers', del_in=5)
+            return
+        media, media_group = [], []
+        async for photo_ in message.client.iter_profile_photos(peer_id, limit=min(int(get_l), 100)):
+            media.append(InputMediaPhoto(pic_.file_id)
+            if len(media) == 10:
+                media_group.append(media)
+                media = []
+        if len(media) != 0:
+            media_group.append(media)
+        if len(media_group) == 0:
+            # Happens if bot doesn't know the user
+            return
+        try:
+            for poto_ in media_group:
+                await userge.send_media_group(chat_id, media=poto_)
+                await asyncio.sleep(2)
+        except FloodWait as e:
+            await asyncio.sleep(e.x + 3)
+        except Exception as err:
+            await CHANNEL.log(f"**ERROR:** `{str(err)}`")
+    else:
+        await send_single(chat_id=chat_id, peer_id=peer_id, pos=1, reply_id=reply_id)

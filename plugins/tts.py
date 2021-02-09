bac15eb42c8a1f3630ab3c8f1b584cfff54f00a4
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2021-02-09 18:09:57 +0530

    Improved TTS

--- plugins/tts.py
+++ plugins/tts.py
@@ -7,38 +7,50 @@ from userge import Message, userge
 
 
 @userge.on_cmd(
-    "tts", about={"header": "Text To Speech", "examples": "{tr}tts en|Userge"}
+    "tts", about={"header": "Text To Speech (default language is en)", 
+    "examples": ["{tr}tts (In reply)", "{tr}tts -en I do as the crystal guides"]}
 )
 async def text_to_speech(message: Message):
     req_file_name = "gtts.mp3"
-    inp_text = message.input_str
-    if ("|" not in inp_text) or not inp_text:
-        await message.edit("Pathetic")
-        return
-    await message.edit("Processing.")
+    reply = message.reply_to_message
+    input_str = message.input_str
     def_lang = "en"
-    def_lang, text = inp_text.split("|")
+    text  = ""
+    if input_str:
+        input_str = input_str.strip()
+        if reply:
+            if (reply.text or reply.caption) and len(input_str.split()) == 1 and input_str.startswith("-"):
+                def_lang = input_str[1:]
+                text = reply.text or reply.caption
+        else:
+            i_split = input_str.split(None, 1)
+            if len(i_split) == 2 and i_split[0].startswith("-"):
+                def_lang = i_split[0][1:]
+                text = i_split[1]
+            else:
+                text = input_str
+    elif reply and (reply.text or reply.caption):
+        text = (reply.text or reply.caption)
+    if not text:
+        await message.err(":: Input Not Found ::\nProvide text to convert to voice !", del_in=7)
+        return
     try:
         await message.edit("Processing..")
-        speeched = gTTS(text, lang=def_lang.strip())
+        speeched = gTTS(text, lang=def_lang)
         speeched.save(req_file_name)
-        await message.edit("Processing...")
         meta = XMan(CPR(req_file_name))
         a_len = 0
-        a_title = "Text To Speech"
-        a_perf = "Google"
-        a_cap = f"Language Code: {def_lang}"
+        a_cap = f"Language Code:  **{def_lang.upper()}**"
         if meta and meta.has("duration"):
             a_len = meta.get("duration").seconds
         await message.edit("Uploading...")
-        await message.reply_audio(
-            audio=req_file_name,
+        await message.client.send_voice(
+            voice=req_file_name,
             caption=a_cap,
             duration=a_len,
-            performer=a_perf,
-            title=a_title,
+            reply_to_message_id=reply.message_id if reply else None
         )
         os.remove(req_file_name)
         await message.delete()
     except Exception as err:
-        await message.edit(err)
+        await message.err(str(err))

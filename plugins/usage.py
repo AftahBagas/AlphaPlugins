d4616bf781f5d97ae63136b8cc33854b04401291
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-30 19:06:13 +0700

    Update usage.py

--- plugins/usage.py
+++ plugins/usage.py
@@ -53,13 +53,13 @@ async def usage(message: Message):
 
     await asyncio.sleep(1.5)
 
-    await message.edit("**Informasi Dyno:**\n\n"
+    await message.edit("⚙️ **Informasi Dyno** ⚙️\n\n"
                        f"┏━━━━━━━━━━━━━━━━━━━━━┓\n"
-                       f" ➥ `Penggunaan Dyno`  **{Config.HEROKU_APP_NAME}**:\n"
-                       f"     •  `{AppHours}`**h**  `{AppMinutes}`**m**  "
+                       f" ⛽ `Penggunaan Dyno`  **{Config.HEROKU_APP_NAME}**:\n"
+                       f"     ➤  `{AppHours}`**h**  `{AppMinutes}`**m**  "
                        f"**|**  [`{AppPercentage}`**%**]"
                        "\n"
-                       " ➥ `Sisa kuota jam Dyno bulan ini`:\n"
-                       f"     •  `{hours}`**h**  `{minutes}`**m**  "
+                       " 🛢️ `Sisa kuota jam Dyno bulan ini`:\n"
+                       f"     ➤  `{hours}`**h**  `{minutes}`**m**  "
                        f"**|**  [`{percentage}`**%**]\n"
                        f"┗━━━━━━━━━━━━━━━━━━━━━┛")

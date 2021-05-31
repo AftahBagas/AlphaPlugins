e76212d1cb985ed57f5633139eba3b91e86bf367
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-31 23:37:32 +0700

    Update calendar.py

--- plugins/calendar.py
+++ plugins/calendar.py
@@ -1,10 +1,10 @@
 import calendar
 from datetime import datetime
 
-from userge import Message, userge
+from alphaz import Message, alphaz
 
 
-@userge.on_cmd(
+@alphaz.on_cmd(
     "cal",
     about={
         "header": "Print calendar of any month of any year.",

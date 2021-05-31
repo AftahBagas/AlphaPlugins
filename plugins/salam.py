5f537a058c8f28c6f254634f9bbb994a01195649
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-31 20:11:48 +0700

    Create salam.py

---
+++ plugins/salam.py
@@ -0,0 +1,22 @@
+"""Animasi AlphaZ Plugins"""
+import asyncio
+from collections import deque
+
+from userge import Message, userge
+
+
+@userge.on_cmd("p$", about={"header": "Salam Dulu"})
+async def tiger_(message: Message):
+    """tiger"""
+    animation_interval = 0.1
+    animation_ttl = range(117)
+    await message.edit("`Assalamu'alaikum`")
+
+
+@userge.on_cmd("l$", about={"header": "Jawab Salam"})
+async def tiger_(message: Message):
+    """tiger"""
+    animation_interval = 0.1
+    animation_ttl = range(117)
+    await message.edit("`Waalaikumsallam`")
+    

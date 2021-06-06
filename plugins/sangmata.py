c5d466405292e5bc5ad2aeb99945ec1109de77db
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-06 09:22:17 +0700

    Update sangmata.py

--- plugins/sangmata.py
+++ plugins/sangmata.py
@@ -3,31 +3,30 @@
 # By @kanjengungsun
 
 from pyrogram.errors.exceptions.bad_request_400 import YouBlockedUser
-from alpha import Message, alpha
+
+from alpha import userge, Message
 from alpha.utils.exceptions import StopConversation
 
 
-@alpha.on_cmd(
-    "sg",
-    about={
-        "header": "Sangmata gives you user's last updated names and usernames.",
-        "flags": {"-u": "To get Username history of a User"},
-        "usage": "{tr}sg [Reply to user]\n" "{tr}sg -u [Reply to user]",
-    },
-)
+@alpha.on_cmd("sg", about={
+    'header': "Sangmata gives you user's last updated names and usernames.",
+    'flags': {
+        '-u': "To get Username history of a User"},
+    'usage': "{tr}sg [Reply to user]\n"
+             "{tr}sg -u [Reply to user]"}, allow_via_bot=False)
 async def sangmata_(message: Message):
-    """Get User's Updated previous Names and Usernames"""
+    """ Dapatkan Nama dan Nama Pengguna Sebelumnya yang Diperbarui Pengguna """
     replied = message.reply_to_message
     if not replied:
-        await message.err("```Balas untuk mendapatkan Nama dan Riwayat Nama Pengguna...```", del_in=5)
+        await message.err("```Balas pesan untuk mendapatkan Nama dan Riwayat Nama Pengguna...```", del_in=5)
         return
     user = replied.from_user.id
     chat = "@Sangmatainfo_bot"
-    await message.edit("```Mendapatkan Informasi Pergantian Nama ...```")
+    await message.edit("```Getting info, Wait plox ...```")
     msgs = []
     ERROR_MSG = "For your kind information, you blocked @Sangmatainfo_bot, Unblock it"
     try:
-        async with alpha.conversation(chat) as conv:
+        async with userge.conversation(chat) as conv:
             try:
                 await conv.send_message("/search_id {}".format(user))
             except YouBlockedUser:
@@ -38,18 +37,16 @@ async def sangmata_(message: Message):
             msgs.append(await conv.get_response(timeout=3, mark_read=True))
     except StopConversation:
         pass
-    name = "📄 **Riwayat Pergantian Nama**"
-    username = "📄 **Riwayat Pergantian Username**"
+    name = "📄 Catatan Pergantian Nama 📄"
+    username = "📄 Catatan Pergantian Usernam 📄"
     for msg in msgs:
-        if "-u" in message.flags:
+        if '-u' in message.flags:
             if msg.text.startswith("Tidak ada catatan yang ditemukan"):
                 await message.edit("```Pengguna tidak pernah mengubah Nama Penggunanya...```", del_in=5)
                 return
             if msg.text.startswith(username):
                 await message.edit(f"`{msg.text}`")
         else:
-            if msg.text.startswith("No records found"):
+            if msg.text.startswith("Tidak ada catatan yang ditemukan"):
                 await message.edit("```Pengguna tidak pernah mengubah Namanya...```", del_in=5)
                 return
-            if msg.text.startswith(name):
-                await message.edit(f"`{msg.text}`")

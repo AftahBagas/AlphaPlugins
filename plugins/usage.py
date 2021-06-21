619f5336cb1734db7ab7e8a812a61d1833e09512
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-21 17:22:56 +0700

    Update usage.py

--- plugins/usage.py
+++ plugins/usage.py
@@ -56,5 +56,5 @@ async def usage(message: Message):
     await message.edit("⚙️ **Informasi Dyno** ⚙️\n\n"
                        f"**Penggunaan Dyno** [`{AppPercentage}`%] **Bulan Ini** =\n"
                        f"     • **{AppHours} hours**\n• **{AppMinutes} minutes**  "
-                       "Sisa Dyno {Config.HEROKU_APP_NAME} [`{percentage}`%] Bulan Ini** =\n"
+                       f"Sisa Dyno {Config.HEROKU_APP_NAME} [`{percentage}`%] Bulan Ini** =\n"
                        f"     • **{hours} hours*"\n• **{minutes} minutes**  ")
